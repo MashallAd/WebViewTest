7ca1c8ab547c7106b0b8c3873752297f8be0523e
zhangxu19 <zhangxu19@baidu.com>
2016-12-28 14:19:18 +0800

    complete open client

--- app/src/main/java/www/baidu/com/zhangxu19/myapplication/ClientOpenActivity.java
+++ app/src/main/java/www/baidu/com/zhangxu19/myapplication/ClientOpenActivity.java
@@ -1,6 +1,11 @@
 package www.baidu.com.zhangxu19.myapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,13 +15,26 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class ClientOpenActivity extends AppCompatActivity {
+import www.baidu.com.zhangxu19.myapplication.databinding.OpenClientPopupViewBinding;
+
+public class ClientOpenActivity extends AppCompatActivity implements IClientOpenPopupBtnClick {
 
     private static final String TAG = ClientOpenActivity.class.getSimpleName();
+
+    private static final String SP_OPEN_CLIENT_MODE_FILE_NAME = "open_client_popup_mode_file_name";
+    private static final String SP_OPEN_CLIENT_MODE = "sp_open_client_mode";
 
     private EditText mEtInput;
     private Button mBtnLoad;
     private WebView mWebView;
+
+    private AlertDialog mDialog;
+
+    private OpenClientPopupViewBinding mOpenClientBinding;
+
+    // set and get
+    //
+    private String curOpenClientUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +59,10 @@ public class ClientOpenActivity extends AppCompatActivity {
         });
 
         initWebView();
+
+        mOpenClientBinding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.open_client_popup_view,
+                null, false);
+        mOpenClientBinding.setHandlers(this);
     }
 
     private void initWebView() {
@@ -49,7 +71,9 @@ public class ClientOpenActivity extends AppCompatActivity {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 Log.d(TAG, "shouldOverrideUrlLoading, url: " + url);
                 if (!url.startsWith("http://") && !url.startsWith("https://")) {
-
+                    curOpenClientUrl = url;
+                    dealOpenClient();
+                    return true;
                 }
                 return super.shouldOverrideUrlLoading(view, url);
             }
@@ -59,6 +83,52 @@ public class ClientOpenActivity extends AppCompatActivity {
         mWebView.getSettings().setJavaScriptEnabled(true);
     }
 
+    private void startActivityLocal() {
+        try {
+            // 以下固定写法
+            final Intent intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(curOpenClientUrl));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        } catch (Exception e) {
+            // 防止没有安装的情况
+            e.printStackTrace();
+        }
+    }
+
+    private void dealOpenClient() {
+
+        // getOpenClientModeInSharedPreference
+        SharedPreferences sp = getSharedPreferences(SP_OPEN_CLIENT_MODE_FILE_NAME, MODE_PRIVATE);
+        String openClientMode = sp.getString(SP_OPEN_CLIENT_MODE,
+                OpenClientPopupMode.ONLY_THIS_TIME_PERMIT.name());
+
+        // dealOpenClientByMode
+        if (openClientMode.equals(OpenClientPopupMode.ALLWAYS_PERMIT.name())) {
+            // if has app, to start
+            startActivityLocal();
+        } else if (openClientMode.equals(OpenClientPopupMode.NO_MORE_HINT.name())) {
+            // nothing to do
+        } else {
+            // default ONLY_THIS_TIME_PERMIT,
+            // open dialog
+            openDialog();
+        }
+
+
+    }
+
+    private void openDialog() {
+        if (mDialog == null) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                    .setView(mOpenClientBinding.getRoot());
+            mDialog = builder.create();
+        }
+        mDialog.show();
+    }
+
     @Override
     public void onBackPressed() {
         if (mWebView.canGoBack()) {
@@ -66,5 +136,35 @@ public class ClientOpenActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public void openOuterClientThisTime(View view) {
+        setOpenClientModeInSP(OpenClientPopupMode.ONLY_THIS_TIME_PERMIT.name());
+        startActivityLocal();
+        mDialog.dismiss();
+    }
+
+    @Override
+    public void openOuterClientAllways(View view) {
+        setOpenClientModeInSP(OpenClientPopupMode.ALLWAYS_PERMIT.name());
+        mDialog.dismiss();
+        startActivityLocal();
+    }
+
+    @Override
+    public void openOuterClientNoMoreHint(View view) {
+        setOpenClientModeInSP(OpenClientPopupMode.NO_MORE_HINT.name());
+        mDialog.dismiss();
+    }
+
+    @Override
+    public void openOuterClientCancel(View view) {
+        mDialog.dismiss();
+    }
+
+    private void setOpenClientModeInSP(String mode) {
+        SharedPreferences sp = getSharedPreferences(SP_OPEN_CLIENT_MODE_FILE_NAME, MODE_PRIVATE);
+        sp.edit().putString(SP_OPEN_CLIENT_MODE, mode).apply();
     }
 }
---
+++ app/src/main/java/www/baidu/com/zhangxu19/myapplication/OpenClientPopupMode.java
@@ -0,0 +1,13 @@
+package www.baidu.com.zhangxu19.myapplication;
+
+/**
+ * Created by zhangxu19 on 16/12/28.
+ */
+
+public enum OpenClientPopupMode {
+
+    ONLY_THIS_TIME_PERMIT,
+    ALLWAYS_PERMIT,
+    NO_MORE_HINT;
+
+}
